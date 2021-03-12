b877035612c9c24a762347183ab539162a136fb2
Wagsnerlund <chelseawagner17@gmail.com>
2021-03-12 01:11:20 +0000

    refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,33 +2,18 @@ const request = require('request');
 const breedName = process.argv[2];
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, resp, body) => {
-  if (error) {
-    return console.log(error);
-  }
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  const breed = data[0]
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`${breedName} does not exist.`);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
 
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      const breed = data[0];
 
+      callback(null, breed);
+    }
+  });
+};
 
-// const hhtps = require('https');
-
-// https.get('https://api.thecatapi.com/v1/breeds/search?q=sib', () => {
-
-// });
-
-// fetch('https://api.thecatapi.com/v1/breeds/search?q=sib')
-// .then(() => {
-
-// });
-
-// console.log(typeof body);
-
+module.exports = { fetchBreedDescription };
